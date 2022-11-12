5634388ffc445085e7c1dec1730451b84eb5a820
Ekaterina599 <82459656+Ekaterina599@users.noreply.github.com>
2022-11-12 20:30:51 +0600

    Update app.kt

--- src/main/kotlin/component/app.kt
+++ src/main/kotlin/component/app.kt
@@ -13,14 +13,17 @@ import react.router.Routes
 import react.router.dom.HashRouter
 import react.router.dom.Link
 
-var colorContext = createContext<List<Color>>()
-val shortOutput = createContext("Full")
+val primer = createContext(Pair("Full", (listOf<Color>())))
 
 val app = FC<Props>("App") {
     val (mode, setMode) = useState("Full")
     val courses = courseList.map { useState(it) }
     val progress = listOf("excellent student", "good student", "loser student")
     var ListColor by useState { listOf(1, 2, 3, 4, 5).map { Color("#0000ff") } }
+    CModePicker {
+        _mode = mode
+        _setMode = setMode
+    }
     CColor {
         listcolor = ListColor
         setColor = { indx, clr ->
@@ -29,41 +32,36 @@ val app = FC<Props>("App") {
             }
         }
     }
-    CModePicker {
-        _mode = mode
-        _setMode = setMode
-    }
-    colorContext.Provider(ListColor) {
-        shortOutput.Provider(mode) {
-            HashRouter { //провайдер инф-и о маршруте
-                 div {
-                    css { display = Display.flex }
-                    courses.map { (_course, _) ->
-                        val name = _course.name
-                        Link {
-                            css { flex = Flex.minContent }
-                            +"Course/$name"
-                            to = "Course/$name"
-                        }
-                    }
-                    progress.map {
-                        val name = it
-                        Link {
-                            css { flex = Flex.minContent }
-                            +"List/$name"
-                            to = "List/$name"
-                        }
+
+    primer.Provider(Pair(mode, ListColor)) {
+        HashRouter { //провайдер инф-и о маршруте
+            div {
+                css { display = Display.flex }
+                courses.map { (_course, _) ->
+                    val name = _course.name
+                    Link {
+                        css { flex = Flex.minContent }
+                        +"Course/$name"
+                        to = "Course/$name"
                     }
                 }
-                Routes {
-                    Route {
-                        path = "Course/:name" //параметр маршрута для курса
-                        element = CCoursePicker.create { this.courses = courses }
+                progress.map {
+                    val name = it
+                    Link {
+                        css { flex = Flex.minContent }
+                        +"List/$name"
+                        to = "List/$name"
                     }
-                    Route {
-                        path = "List/:name" //параметр маршрута для курса
-                        element = CGradesPicker.create { this.courseState = courses }
-                    }
+                }
+            }
+            Routes {
+                Route {
+                    path = "Course/:name" //параметр маршрута для курса
+                    element = CCoursePicker.create { this.courses = courses }
+                }
+                Route {
+                    path = "List/:name" //параметр маршрута для курса
+                    element = CGradesPicker.create { this.courseState = courses }
                 }
             }
         }
